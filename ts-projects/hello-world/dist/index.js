619c483cba8ded8cac2ca9969a68c6052456d5f1
Ricardo Leyva <ricardomtzleyva@hotmail.com>
2025-07-13 00:07:23 -0400

    Advanced types

--- ts-projects/hello-world/dist/index.js
+++ ts-projects/hello-world/dist/index.js
@@ -1,28 +1,5 @@
 "use strict";
-let sales = 1234563982;
-let course = "Typescript";
-let is_published = true;
-let level;
-let a = 100;
-let b = "Coffee";
-let c = [true, false, false];
-let d = { age: 5 };
-let e = [3];
-let f;
-let g = [];
-function render(document) {
-    console.log(document);
-}
-let numbers = [1, 2, 3];
-let user = [1, "Ric"];
-let mySize = 2;
-console.log(mySize);
-function calculateTax(income, taxYear = 2022) {
-    if (taxYear < 2020)
-        return income * 0.4;
-    return income * 0.2;
-}
-console.log(calculateTax(10000, 2019));
+var _a;
 let employee = {
     id: 1,
     name: "",
@@ -30,6 +7,58 @@ let employee = {
         console.log(date);
     },
 };
-employee.name = "Ric";
-console.log(employee);
+function kgToLbs(weight) {
+    if (typeof weight === "number")
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+kgToLbs(10);
+kgToLbs("10kg");
+let textBox = {
+    drag() { },
+    resize() { },
+};
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log("Hola");
+}
+greet("hello world");
+greet(null);
+greet(undefined);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let log = null;
+log === null || log === void 0 ? void 0 : log("a");
+let customer = getCustomer(1);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+let speed = null;
+let ride = {
+    speed: speed !== null && speed !== void 0 ? speed : 30,
+};
+function render(document) {
+    if (typeof document === "string") {
+        document.toUpperCase();
+    }
+}
+let user1 = {
+    name: "John Smith",
+    age: 30,
+    occupation: "Software Engineer",
+};
+let user2 = {
+    name: "Kate Muller",
+    age: 28,
+};
+function getUser() {
+    let data = {
+        address: { street: undefined },
+    };
+    return data;
+}
+let user = getUser();
+console.log(user && user.address ? user.address.street : undefined);
 //# sourceMappingURL=index.js.map
--- ts-projects/hello-world/src/index.ts
+++ ts-projects/hello-world/src/index.ts
@@ -2,55 +2,218 @@
 // if (age < 50) age += 10;
 // console.log(age);
 
-let sales = 123_456_3982;
-let course = "Typescript";
-let is_published = true;
-let level; // Avoid using type 'any'
-let a = 100;
-let b = "Coffee";
-let c = [true, false, false];
-let d = { age: 5 };
-let e = [3];
-let f;
-let g = [];
-function render(document: any) {
-  console.log(document);
-}
-//Array
-let numbers: number[] = [1, 2, 3];
+// let sales = 123_456_3982;
+// let course = "Typescript";
+// let is_published = true;
+// let level; // Avoid using type 'any'
+// let a = 100;
+// let b = "Coffee";
+// let c = [true, false, false];
+// let d = { age: 5 };
+// let e = [3];
+// let f;
+// let g = [];
+// function render(document: any) {
+//   console.log(document);
+// }
+// //Array
+// let numbers: number[] = [1, 2, 3];
 
-//Tuple (Better if used only 2 at a time as key pairs)
-let user: [number, string] = [1, "Ric"];
+// //Tuple (Better if used only 2 at a time as key pairs)
+// let user: [number, string] = [1, "Ric"];
 
-//Enums
-const enum Size {
-  Small = 1,
-  Medium,
-  Large,
-}
-let mySize: Size = Size.Medium;
-console.log(mySize);
+// //Enums
+// const enum Size {
+//   Small = 1,
+//   Medium,
+//   Large,
+// }
+// let mySize: Size = Size.Medium;
+// console.log(mySize);
 
-//Functions (Better is type of return is coded, if no return then void)
-function calculateTax(income: number, taxYear = 2022): number {
-  if (taxYear < 2020) return income * 0.4;
-  return income * 0.2;
-}
+// //Functions (Better is type of return is coded, if no return then void)
+// function calculateTax(income: number, taxYear = 2022): number {
+//   if (taxYear < 2020) return income * 0.4;
+//   return income * 0.2;
+// }
 
-console.log(calculateTax(10_000, 2019));
+// console.log(calculateTax(10_000, 2019));
 
-//Objects
-let employee: {
+// //Objects
+// let employee: {
+//   readonly id: number; //Readonly cannot be modified
+//   name: string;
+//   fax?: string;
+//   retire: (date: Date) => void;
+// } = {
+//   id: 1,
+//   name: "",
+//   retire: (date: Date) => {
+//     console.log(date);
+//   },
+// };
+// employee.name = "Ric";
+// console.log(employee);
+
+// Type Alias
+type Employee = {
   readonly id: number; //Readonly cannot be modified
   name: string;
   fax?: string;
   retire: (date: Date) => void;
-} = {
+};
+
+let employee: Employee = {
   id: 1,
   name: "",
   retire: (date: Date) => {
     console.log(date);
   },
 };
-employee.name = "Ric";
-console.log(employee);
+
+//Union types
+function kgToLbs(weight: number | string): number {
+  // Narrowing
+  if (typeof weight === "number") return weight * 2.2;
+  else return parseInt(weight) * 2.2;
+}
+
+kgToLbs(10);
+kgToLbs("10kg");
+
+//Intersection types
+type Draggable = {
+  drag: () => void;
+};
+
+type Resizable = {
+  resize: () => void;
+};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+  drag() {},
+  resize() {},
+};
+
+//Literal types (exact or specific value)
+type Quantity = 50 | 100;
+type Metric = "cm" | "inch";
+
+//Nullable types
+function greet(name: string | null | undefined) {
+  if (name) console.log(name.toUpperCase());
+  else console.log("Hola");
+}
+
+greet("hello world");
+greet(null);
+greet(undefined);
+
+//Optional chaining
+type Customer = {
+  birthday?: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { birthday: new Date() };
+}
+
+//Optional element access operator
+// customers?.[0]
+
+//Optional call
+let log: any = null;
+log?.("a");
+
+let customer = getCustomer(1);
+console.log(customer?.birthday?.getFullYear()); //Optional property access operator
+
+//The nullish coaelscing operator
+let speed: number | null = null;
+//Falsy values --> (undefined, null, '', false, 0)
+let ride = {
+  speed: speed ?? 30,
+};
+
+//Type assertions
+// let phone = document.getElementById("phone") as HTMLInputElement;
+// let address = <HTMLInputElement>document.getElementById("address");
+
+//Unknown type (better than any so you have to call the methods in the object)
+function render(document: unknown) {
+  // Narrowing
+  if (typeof document === "string") {
+    document.toUpperCase();
+  }
+}
+
+//Never type
+// function reject(message: string): never {
+//   throw new Error(message);
+// }
+
+// function processEvents(): never {
+//   while (true) {
+//     //Read a message from a queue
+//   }
+// }
+
+// reject("hola");
+// processEvents();
+// console.log("Hello World");
+
+//Excercises
+// A
+type Users = {
+  name: string;
+  age: number;
+  occupation?: string;
+};
+let user1: Users = {
+  name: "John Smith",
+  age: 30,
+  occupation: "Software Engineer",
+};
+let user2: Users = {
+  name: "Kate Muller",
+  age: 28,
+};
+
+// B
+type Bird = {
+  fly: () => void;
+};
+type Fish = {
+  swim: () => void;
+};
+type Pet = Bird | Fish;
+
+// C
+type DaysOfWeek =
+  | "Monday"
+  | "Tuesday"
+  | "Wednesday"
+  | "Thursday"
+  | "Friday"
+  | "Saturday"
+  | "Sunday";
+
+let day: DaysOfWeek = "Monday";
+// D
+function getUser() {
+  let data = {
+    address: { street: undefined },
+  };
+  return data;
+}
+let user = getUser();
+console.log(user && user.address ? user.address.street : undefined);
+console.log(user?.address?.street);
+
+function bar(){
+
+}
+let foo = ()=>void;
+let x = foo !== null && foo !== undefined ? foo: bar(); 
